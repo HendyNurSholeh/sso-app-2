456d8783b77da497a5c719fd6eda924b7b0d202f
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2025-12-29 09:52:38 +0800

    feat

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
     <title>Dashboard</title>
 </head>
 <body>
-    <h1>Dashboard</h1>
+    <h1>Dashboard Aplikasi 2</h1>
     <p>Login berhasil via SSO</p>
 
     <p>User: {{ auth()->user()->name }}</p>
