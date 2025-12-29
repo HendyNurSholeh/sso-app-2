a902d994865aa4cd0acf0ea2311ba87d96b82454
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2025-12-29 11:53:50 +0800

    feat

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\SSOController;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
